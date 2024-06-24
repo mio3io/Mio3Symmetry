e9f78ef365f148e851f982a605794ea5c0fe841c
mio <2270244+mio3io@users.noreply.github.com>
2024-06-24 17:28:01 +0900

    LR表情の非対称化オプション追加

--- __init__.py
+++ __init__.py
@@ -34,21 +34,28 @@ class MIO3_OT_quick_symmetrize(Operator):
             ("-X", "-X → +X", ""),
         ],
     )
+    facial: BoolProperty(name="UnSymmetrize L/R Facial ShapeKeys", default=False)
     normal: BoolProperty(name="Normal", default=True)
     uvmap: BoolProperty(name="UVMap", default=True)
     center: BoolProperty(name="Origin to Center", default=True)
     remove_mirror_mod: BoolProperty(name="Remove Mirror Modifier", default=True)
 
-    threshold = 0.0001
     suffixes = [
-        ("_r", ".r", "-r", " r", "right", "_R", ".R", "-R", " R", "Right"),
-        ("_l", ".l", "-l", " l", "left", "_L", ".L", "-L", " L", "Left"),
+        ("_r", ".r", "-r", " r", "_R", ".R", "-R", " R", "Right"),
+        ("_l", ".l", "-l", " l", "_L", ".L", "-L", " L", "Left"),
     ]
 
     main_verts = []
     sub_verts = []
     original_cursor_location = None
     original_active_shape_key_index = None
+
+    replace_names = {
+        "ウィンク": "MMD_Wink_R",
+        "ウィンク右": "MMD_Wink_L",
+        "ウィンク２": "MMD_Wink2_R",
+        "ｳｨﾝｸ２右": "MMD_Wink2_L",
+    }
 
     @classmethod
     def poll(cls, context):
@@ -118,7 +125,7 @@ class MIO3_OT_quick_symmetrize(Operator):
             input=bm.verts[:] + bm.edges[:] + bm.faces[:],
             direction="X" if self.mode == "+X" else "-X",
             use_shapekey=True,
-            dist=0.0001,
+            dist=0.00001,
         )
 
         for elem in bm.verts[:] + bm.edges[:] + bm.faces[:]:
@@ -132,9 +139,13 @@ class MIO3_OT_quick_symmetrize(Operator):
 
         if self.uvmap:
             self.symm_uv(bm)
+
         self.symm_vgroups()
 
         bmesh.update_edit_mesh(self.obj.data)
+
+        if self.facial:
+            self.unsymm_facial()
 
         if self.normal:
             self.create_temp_vgroup()
@@ -208,9 +219,7 @@ class MIO3_OT_quick_symmetrize(Operator):
     def symm_vgroups(self):
         vgroups = self.obj.vertex_groups
         suffixes = self.suffixes[0] if self.mode == "+X" else self.suffixes[1]
-        suffixes = [
-            name for name in vgroups.keys() if name.endswith(suffixes, True)
-        ]
+        suffixes = [name for name in vgroups.keys() if name.endswith(suffixes, True)]
         for suffix in suffixes:
             from_group = vgroups.get(suffix)
             if from_group:
@@ -235,9 +244,70 @@ class MIO3_OT_quick_symmetrize(Operator):
             self.orgcopy.scale[0] *= -1
             pass
 
+    # 表情の非対称化
+    def unsymm_facial(self):
+        obj = self.obj
+        original_use_mesh_mirror_x = obj.use_mesh_mirror_x
+        obj.use_mesh_mirror_x = False
+        key_blocks = obj.data.shape_keys.key_blocks
+
+        if not obj.data.total_vert_sel:
+            return
+
+
+        if not key_blocks:
+            return
+
+        side_suffixes = {
+            "+X": (self.suffixes[0], self.suffixes[1]),
+            "-X": (self.suffixes[1], self.suffixes[0]),
+        }
+        target_suffixes, source_suffixes = side_suffixes[self.mode]
+
+        basis = key_blocks[0]
+        self.rename_shape_keys(obj, self.replace_names)
+        try:
+            for i, shape_key in enumerate(key_blocks):
+                for target_suffix, source_suffix in zip(
+                    target_suffixes, source_suffixes
+                ):
+                    if shape_key.name.endswith(target_suffix):
+                        source_name = (
+                            shape_key.name[: -len(target_suffix)] + source_suffix
+                        )
+                        if source_name in key_blocks:
+                            obj.active_shape_key_index = i
+                            bpy.ops.mesh.blend_from_shape(
+                                shape=source_name, blend=1, add=False
+                            )
+                            obj.active_shape_key_index = key_blocks.find(source_name)
+                            bpy.ops.mesh.blend_from_shape(
+                                shape=basis.name, blend=1, add=False
+                            )
+                            break
+            
+            # Basisデータが変わる対処 あとで変更
+            obj.active_shape_key_index = 0
+            bpy.ops.mesh.blend_from_shape(
+                shape=basis.name, blend=1, add=False
+            )
+        finally:
+            reverse_names = {v: k for k, v in self.replace_names.items()}
+            self.rename_shape_keys(obj, reverse_names)
+            obj.use_mesh_mirror_x = original_use_mesh_mirror_x
+
+
+    def rename_shape_keys(self, obj, dicts):
+        if obj.data.shape_keys:
+            for key in obj.data.shape_keys.key_blocks:
+                if key.name in dicts:
+                    key.name = dicts[key.name]
+
+
     def draw(self, context):
         layout = self.layout
         layout.prop(self, "mode")
+        layout.prop(self, "facial")
         layout.prop(self, "normal")
         layout.prop(self, "uvmap")
         layout.prop(self, "center")
@@ -256,6 +326,7 @@ translation_dict = {
     "ja_JP": {
         ("*", "Quick Symmetry"): "対称化＆リカバリー",
         ("*", "Symmetrize meshes, shape keys, vertex groups, UVs, and normals"): "メッシュ・シェイプキー・頂点グループ・UV・法線を対称化",
+        ("*", "UnSymmetrize L/R Facial ShapeKeys"): "L/Rの表情シェイプキーを非対称化",
         ("*", "Remove Mirror Modifier"): "ミラーモディファイアを削除",
         ("*", "Origin to Center"): "原点を基準に対称化",
         ("*", "Object is not a mesh"): "オブジェクトがメッシュではありません",
